437f731f9e7bef32108755d8cc3d7d14e5e94106
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-05-02 23:00:32 -0300

    allow cors to single route

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const cors = require('cors')
 
 // Import key
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
-router.get('/', async (req, res) => {
+router.get('/', cors(), async (req, res) => {
     try {
         // generate API parameters
         const params = new URLSearchParams({
