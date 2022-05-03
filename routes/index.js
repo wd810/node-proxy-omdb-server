857485fbea36808b5840b9273c8b86a263c3145b
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-05-02 22:36:45 -0300

    get data from OMDB

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,32 @@ const router = express.Router()
 const needle = require('needle')
 
 // Import key
-const API_BASE_URL = process.API_BASE_URL
-const API_KEY_NAME = process.API_KEY_NAME
-const API_KEY_VALUE = process.API_KEY_VALUE
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
 
 router.get('/', async (req, res) => {
-    res.json({ success: true })
+    try {
+        // generate API parameters
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE,
+            // parse parameters under server console
+            ...url.parse(req.url, true).query
+        })
+
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+        const data =  await apiRes.body
+
+        //  log under dev environment
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`${API_BASE_URL}?${params}`)
+        }
+
+        res.status(200).json({ data })
+
+    } catch (error) {
+        res.status(500).json({ error })
+    }
 })
 
 module.exports = router
