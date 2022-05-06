596ec955e9dee9bc2acdb52a610cd28a83dd01bf
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-05-05 23:02:56 -0300

    every 10 minutes, max click is 5 times

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// rate limit
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mimutes
+    max: 5
+});
+app.use(limiter);
 
 app.set('trust proxy', 1);
 
